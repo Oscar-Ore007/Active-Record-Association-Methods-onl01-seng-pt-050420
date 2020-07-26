16fcb88eb8a7911dcdcd6fdec141cd8904ed79d0
Oscar Ore <oscar.ore2011@gmail.com>
2020-07-26 19:41:34 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,16 +1,20 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
+  
+  def get_first_song 
+    self.get_first_song 
+  end 
 
   def get_genre_of_first_song
-    self.songs.first 
+    self.songs.first.genre 
   end
 
   def song_count
-    self.songs.first 
+    self.songs.count 
   end
 
   def genre_count
-    #return the number of genres associated with the artist
+    self.genres.count 
   end
 end
